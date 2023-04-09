b711475daa122b1c3fb9add822cf5dbafaa99de3
Vikas Pathak <123vikas.vp79@gmail.com>
2023-04-09 15:22:33 +0530

    wild card 0.0.0.0 again

--- Backend/src/app.mjs
+++ Backend/src/app.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-// import todoRoutes from './todo.mjs';
+import todoRoutes from './todo.mjs';
 import todoRoutesMDB from './todoMongoDB.mjs';
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.use('/todos', todoRoutes);
+app.use('/todos', todoRoutes);
 app.use('/todomdb', todoRoutesMDB);
 
 export default app;
--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -1,5 +1,14 @@
 import app from './app.mjs'
+import dotenv from 'dotenv';
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+
+dotenv.config();
+
+const hostname = process.env.HOSTNAME||"localhost";
+const port = process.env.PORT||8080; 
+// console.log(hostname);
+// console.log(port);
+ 
+app.listen(port,hostname, () => {
+  console.log(`Server running at http://${hostname}/:${port}/`);
+});   
